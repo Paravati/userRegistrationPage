5321c3fbcbbd864851575813968c283c9854f40f
Patrycja <pati.iwanek@gmail.com>
2021-05-10 21:36:20 +0200

    new signup view to create an account

--- userRegistration/userRegistration/urls.py
+++ userRegistration/userRegistration/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView # new
+from .views import SignUpView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+
 ]
